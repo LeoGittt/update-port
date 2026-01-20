3f518957c6e5a49474c7f668edaafc6c34564a8b
Leonel Gonzalez <lg606138@gmail.com>
2026-01-20 11:53:47 -0300

    update
    sd

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -122,14 +122,14 @@ export default function Hero({ isVisible }: HeroProps) {
                 <ArrowRight className="w-4 h-4 relative z-20 group-hover:translate-x-1 transition-transform duration-500" />
               </Link>
               
-              {/* <Link
+              <Link
                 href="/CV_Leonel_Gonzalez.pdf"
                 target="_blank"
                 className="group h-11 w-48 sm:h-14 sm:w-56 border border-white/10 hover:border-white/20 text-gray-400 hover:text-white text-[9px] sm:text-[10px] font-black tracking-widest rounded-xl transition-all duration-300 flex items-center justify-center gap-3"
               >
                 <span>VER MI CV</span>
                 <FileText className="w-4 h-4" />
-              </Link> */}
+              </Link>
             </motion.div>
 
             {/* Social & Location Micro Footer */}
